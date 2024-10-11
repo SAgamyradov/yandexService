9607e52347ad97a4cdbed37ee14558e407a61f8b
SAgamyradov <agamyradowserdar777@gmail.com>
2024-10-11 17:12:59 +0300

    spr1/inc5

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 	"strings"
 )
 
@@ -11,8 +12,22 @@ type Config struct {
 }
 
 func InitConfig() *Config {
-	addr := flag.String("a", "localhost:8080", "Адрес запуска HTTP-сервера")
-	baseURL := flag.String("b", "http://localhost:8080/", "baseURL address for short")
+
+	defaultAddr := "localhost:8080"
+	defaultBaseURL := "http://localhost:8080/"
+
+	addrEnv := os.Getenv("SERVER_ADDRESS")
+	baseURLEnv := os.Getenv("BASE_URL")
+
+	if addrEnv == "" {
+		addrEnv = defaultAddr
+	}
+	if baseURLEnv == "" {
+		baseURLEnv = defaultBaseURL
+	}
+
+	addr := flag.String("a", addrEnv, "Адрес запуска HTTP-сервера")
+	baseURL := flag.String("b", baseURLEnv, "baseURL address for short")
 
 	flag.Parse()
 	if !strings.HasSuffix(*baseURL, "/") {
